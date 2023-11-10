471eb4b6f5e0cd387a35f33b445123ceb733534f
arch <elsavant>
2023-11-10 11:53:23 +0000

    Classified Text Email as Spam or Not Spam

--- Flask/app.py
+++ Flask/app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, request
+import pickle
+
+tokenizer = pickle.load(open("models/cv.pkl", "rb"))
+model = pickle.load(open("models/clf.pkl", "rb"))
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=["GET", "POST"])
 def home():
-    text = ""
-    if request.method == "POST":
-        text = request.form.get('email-content')
-    return render_template("home.html", text=text)
+    return render_template("home.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    email_text = request.form.get("email-content")
+    tokenize_email = tokenizer.transform([email_text])
+    prediction = model.predict(tokenize_email)
+    prediction = "spam" if prediction == 1 else "not spam"
+    return render_template("home.html", prediction=prediction,
+                           text=email_text)
 
 
 if __name__ == "__main__":
